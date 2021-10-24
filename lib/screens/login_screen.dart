99fca3d2279699b3cc478aedbabd8c1ee26ef828
dianyss2011 <89366318+dianyss2011@users.noreply.github.com>
2021-10-24 15:10:42 -0500

    Update login_screen.dart

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({ Key? key }) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+   String _email = '';
+
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+  child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            _showLogo(),
+            _showEmail(),
+          ],
+        )
+      ),
+    );
+  }
+
+  Widget _showLogo() {
+    return Image(
+      image: AssetImage('assets/vehicles_logo.png'),
+      width: 300,
+    );
+  }
+
+  Widget _showEmail() {
+    return Container(
+      padding: EdgeInsets.all(20),
+      child: TextField(
+        autofocus: true,
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+          hintText: 'Ingresa tu email...',
+          labelText: 'Email',
+          suffixIcon: Icon(Icons.email),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10)
+          ),
+        ),
+        onChanged: (value) {
+          _email = value;
+          print(_email);
+        },
+      ),
+    );
+  }

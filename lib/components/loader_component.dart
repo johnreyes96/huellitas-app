b4cdf14646fb853b3349803e6bad0d0efc137684
johnfreyes <jhonfer96@hotmail.com>
2021-11-29 01:24:55 -0500

    change loader component design

--- lib/components/loader_component.dart
+++ lib/components/loader_component.dart
@@ -13,18 +13,27 @@ class LoaderComponent extends StatelessWidget {
         width: 200,
         height: 100,
         decoration: BoxDecoration(
-          color: Colors.yellow[300],
+          color: const Color(0xFF004489),
           borderRadius: BorderRadius.circular(10)
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const CircularProgressIndicator(),
-            const SizedBox(height: 20,),
-            Text(text, style: const TextStyle(fontSize: 20),),
-          ],
-        ),
-      ),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(height: 10),
+              const CircularProgressIndicator(),
+              const SizedBox(height: 20,),
+              Text(
+                text,
+                style: const TextStyle(
+                  fontSize: 20,
+                  color: Colors.white
+                )
+              )
+            ]
+          ),
+        )
+      )
     );
   }
 } 
